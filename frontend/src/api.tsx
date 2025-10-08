e8672e2d5df083bfbbb8b028ce26723a2bb80c4c
AayushMallya <aayushmallya@outlook.com>
2025-10-07 21:39:04 -0400

    dash boiler

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 
 
@@ -12,7 +12,7 @@ export const searchCompanies = async (query: string) => {
 
     try{
         const data = await axios.get<SearchResponse>(
-             `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}` 
+             `https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_APP_API_KEY}` 
         );
         return data;
     } catch (error) {
@@ -25,4 +25,14 @@ export const searchCompanies = async (query: string) => {
         }
         
     }
+};
+
+
+export const getCompanyProfile = async (query: string) => {
+    try{
+        const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`)
+        return data;
+    } catch (error: any) {
+        console.log("error message from PAI: ", error.message);
+}
 };
