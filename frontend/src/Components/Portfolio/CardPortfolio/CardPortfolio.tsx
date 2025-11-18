58cd4e811428ca021aab2a0f1916e37a78bcb6a7
AayushMallya <aayushmallya@outlook.com>
2025-11-18 18:46:54 -0500

    frnt

--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const CardPortfolio = ({portfolioValue, onPortfolioDelete}: Props) => {
   return (
-     <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3 relative bg-mint/50">
+     <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-[0_0_20px_rgba(255,215,0,0.8)] md:w-1/3 relative bg-gray-100/80 border-4 border-gold/90 ">
       <Link to = {`/company/${portfolioValue}/company-profile`} className="pt-6 text-xl font-bold">{portfolioValue}</Link>
     <DeletePortfolio onPortfolioDelete = {onPortfolioDelete} portfolioValue={portfolioValue} />
     
